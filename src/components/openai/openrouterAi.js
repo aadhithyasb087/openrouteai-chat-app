7bac3b5bc4bf7d5797702151a7f7abb105cc94ea
aadhithyasb087 <152068655+aadhithyasb087@users.noreply.github.com>
2024-08-11 00:33:14 +0530

    Update openrouterAi.js

--- src/components/openai/openrouterAi.js
+++ src/components/openai/openrouterAi.js
@@ -1,11 +1,10 @@
 import OpenAI from "openai";
 
-// const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY;
 // console.log(apiKey);
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
-  apiKey:
-    "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",
+  apiKey:`${apiKey}`,
   // defaultHeaders: {
   //   "HTTP-Referer": $YOUR_SITE_URL, // Optional, for including your app on openrouter.ai rankings.
   //   "X-Title": $YOUR_SITE_NAME, // Optional. Shows in rankings on openrouter.ai.
